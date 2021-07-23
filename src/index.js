9846888bb88b1a356aa927e5cc2c10af72d6048a
Alex Gabriel <bgabriel@etsy.com>
2021-07-22 20:48:46 -0500

    Adding initial state

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import AppRouter from './routes/AppRouter';
-
-const initialState = {};
+import { initialState } from './initialState';
 
 const store = createStore(reducer, initialState);
 
 ReactDOM.render(
   <Provider>
     <AppRouter />
-
   </Provider>
   , document.getElementById('app'),
 );
